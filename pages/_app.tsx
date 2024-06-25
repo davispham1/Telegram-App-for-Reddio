23bf07af1805501de00c6106e3e78a8c9c51c181
lchin <l.chin@ufl.edu>
2024-06-25 16:27:38 +0800

    Deleted default code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
   sepolia,
 } from 'wagmi/chains';
 import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
@@ -11,7 +17,14 @@ import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
-  chains: [sepolia],
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
   ssr: true,
 });
 
@@ -21,7 +34,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
-        <RainbowKitProvider>
+        <RainbowKitProvider modalSize="compact" initialChain={sepolia}>
           <Component {...pageProps} />
         </RainbowKitProvider>
       </QueryClientProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
-
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <ConnectButton/>
+      <ConnectButton/>;
     </div>
   );
 };
