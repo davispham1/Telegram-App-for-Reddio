a30550c6444d224ef2437789441af2e1a3bcec2f
davispham1 <davispham1126@gmail.com>
2024-06-27 16:37:50 +0800

    major changes to dependencies

---
+++ pages/components/Deposit.tsx
@@ -0,0 +1,32 @@
+import React, {useCallback, useState} from "react";
+import { depositUSDC } from "./config";
+
+export default function Deposit() {
+    const [amount, setAmount] = useState('')
+    const deposit = useCallback(
+        async (type: any) => {
+            try {
+                await depositUSDC(Number(amount))
+            } catch (e) {
+                console.log(e);
+            }
+        },
+        [amount],
+    );
+    return <div>
+    <p className="Text">
+        You can deposit USDC to your account here.
+    </p>
+    <fieldset className="Fieldset">
+        <label className="Label" htmlFor="amount">
+            Amount
+        </label>
+        <input className="Input" id="amount" onChange={e => setAmount(e.target.value) } />
+    </fieldset>
+    <div
+        style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}
+    >
+        <button className="Button green" onClick={deposit}>Deposit</button>
+    </div>
+</div>
+}
