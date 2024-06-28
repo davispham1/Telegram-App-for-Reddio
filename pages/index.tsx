03b8cd7424971060ec4b769090fdc49afb50e700
davispham1 <davispham1126@gmail.com>
2024-06-28 15:31:39 +0800

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,35 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import BsTabs from './components/tabs';
 import { useEffect } from 'react';
+import { initReddio } from './components/config';
 
 const Home: NextPage = () => {
   useEffect(() => {
+    initReddio()
     import('bootstrap/dist/js/bootstrap.bundle.min.js')
-      .catch(err => console.log("Error loading Bootstrap", err));
+      .catch(err => console.log("Error with Bootstrap", err));
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-web-app.js';
+    script.async = true;
+    script.onload = () => {
+      const tele = window.Telegram;
+      if (tele && tele.WebApp) {
+        tele.WebApp.ready();
+      } else {
+        console.error('Telegram.WebApp is not defined');
+      }
+    };
   }, []);
   return (
     <>
-      <div className='Walletbutton'>
+      <header className='Walletbutton'>
         <ConnectButton
           accountStatus={{
             smallScreen: 'avatar',
             largeScreen: 'full',
-          }}
+          }}  
         />
-      </div>
+      </header>
       <BsTabs />
     </>
   );
