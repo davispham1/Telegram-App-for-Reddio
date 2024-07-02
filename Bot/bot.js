22fc58f92f06b65ad1563e868e1793c695d1521d
davispham1 <140675801+davispham1@users.noreply.github.com>
2024-07-02 05:27:35 -0400

    Update bot.js

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 const { webApp } = require('telegraf/typings/button')
 
-const TOKEN = '7389090595:AAEWs8ZNsT_cjboa8RJMEHBUE6sFYXSs7iE'
+const TOKEN = 
 const bot = new Telegraf(TOKEN)
 
 bot.start((ctx) => ctx.reply('Welcome'), webApp())
