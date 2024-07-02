6a4ae01e1f704f376af4cb17d15e17e642cf1d14
davispham1 <davispham1126@gmail.com>
2024-07-02 15:44:23 +0800

    tabs

--- pages/components/tabs.tsx
+++ pages/components/tabs.tsx
@@ -1,34 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Deposit from "./Deposit";
 
 
 const BsTabs: React.FC = () => {
-    return ( 
+    const [activeTab, setActiveTab] = useState<string>('deposit');
+
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab);
+  };
+
+    return (
         <div className="d-flex justify-content-center align-items-center vh-100">
-            <div className="container-box">
-                <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                    <li className="nav-item" role="presentation">
-                        <button className="nav-link" id="pills-Deposit-tab" data-bs-toggle="pill" data-bs-target="#pills-Deposit" type="button" role="tab" aria-controls="pills-Deposit" aria-selected="false">Deposit</button>
-                    </li>
-                    <li className="nav-item dropdown" role="presentation">
-                        <button className="btn btn-primary dropdown-toggle" type="button" id="dropdownWithdraw" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Withdraw
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="dropdownWithdraw">
-                            <a className="dropdown-item" href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                </ul>
-                <div className="tab-content" id="pills-tabContent">
-                    <div className="tab-pane fade" id="pills-Deposit" role="tabpanel" aria-labelledby="pills-Deposit-tab" tabIndex={0}><Deposit /></div>
-                    <div className="tab-pane fade" id="pills-Withdraw" role="tabpanel" aria-labelledby="pills-Withdraw-tab" tabIndex={0}></div>
-                </div>
-            </div>
+      <div className="container-box">
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <a
+              className={`nav-link ${activeTab === 'deposit' ? 'active' : ''}`}
+              onClick={() => handleTabChange('deposit')}
+              aria-current="page"
+              href="#"
+            >
+              Deposit
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className={`nav-link dropdown-toggle ${activeTab === 'withdraw' ? 'active' : ''}`}
+              onClick={() => handleTabChange('withdraw')}
+              data-bs-toggle="dropdown"
+              href="#"
+              role="button"
+              aria-expanded="false"
+            >
+              Withdraw
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Action</a></li>
+              <li><a className="dropdown-item" href="#">Another action</a></li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+        <div className="tab-content" id="pills-tabContent">
+          <div className={`tab-pane fade ${activeTab === 'deposit' ? 'show active' : ''}`} id="pills-Deposit" role="tabpanel" aria-labelledby="pills-Deposit-tab">
+            <Deposit />
+          </div>
+          <div className={`tab-pane fade ${activeTab === 'withdraw' ? 'show active' : ''}`} id="pills-Withdraw" role="tabpanel" aria-labelledby="pills-Withdraw-tab">
+            Content for Withdraw tab
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default BsTabs;
