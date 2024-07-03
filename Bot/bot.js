a40593882ec81b893ee7dd2e2efcba9e4cd31671
davispham1 <davispham1126@gmail.com>
2024-07-03 14:50:43 +0800

    styling

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,12 +1,10 @@
 const { Telegraf } = require('telegraf')
-const { webApp } = require('telegraf/typings/button')
-
 const TOKEN = '7389090595:AAEWs8ZNsT_cjboa8RJMEHBUE6sFYXSs7iE'
 const bot = new Telegraf(TOKEN)
 
-const web_link = ''
+const web_link = "https://telegram-app-for-reddio.vercel.app/"
 
-bot.start((ctx) => ctx.reply('Welcome', {reply_markup:{keyboard:[[{text:"web app", web_app:web_link}]]}
+bot.start((ctx) => ctx.reply('Welcome', {reply_markup:{keyboard:[[{text:"web app", web_app: { url: web_link} }]]},
 })
 )
 
